4fc2fb522f54d7620c9859ef9554bfbe3b53c969
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-09-03 22:30:40 +0530

    Fixed

---
+++ src/controllers/Employer/employer.removeJob.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { JobListing } from "../../models/jobListings.model";
+import { User } from "../../models/auth.model";
+
+export const createJob = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user.id;
+    const jobId = req.params.jobId;
+
+    const user = await User.findById(userId);
+    console.log(user?.role);
+    if (user?.role !== "EMPLOYER") {
+      return res.status(404).json({
+        status: "fail",
+        message: "No autherized!",
+      });
+    }
+
+    await JobListing.findByIdAndDelete(jobId);
+
+    res.status(204).json({
+      status: "success",
+      message: "Deleted Successfully!",
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
